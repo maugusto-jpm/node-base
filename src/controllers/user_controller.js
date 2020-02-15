c264d35eecb91fd464ea2d615b3ef441639f5fe1
Marcelo Nascimento <maugusto.jpm@gmail.com>
2020-02-14 21:38:30 -0300

    Added creation support

--- src/controllers/user_controller.js
+++ src/controllers/user_controller.js
@@ -1,9 +1,17 @@
 const { User } = require('../app/models');
 
 module.exports = {
-  async index(_, res) {
+  async index(req, res) {
     const users = await User.findAll();
 
     return res.json(users);
   },
+
+  async store(req, res) {
+    const { name, email, password } = req.body;
+
+    const user = await User.create({ name, email, password_hash: password });
+
+    return res.json(user);
+  }
 };
--- src/routes/models/user.js
+++ src/routes/models/user.js
@@ -3,5 +3,6 @@ const router = require('express').Router();
 const UserController = require('../../controllers/user_controller');
 
 router.get('/', UserController.index);
+router.post('/', UserController.store);
 
 module.exports = router;
