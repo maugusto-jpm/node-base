7d391038658c149c1ae7b05ba112909fc997bf98
Marcelo Nascimento <maugusto.jpm@gmail.com>
2020-02-14 22:43:18 -0300

    Added test support

--- Dockerfile
+++ Dockerfile
@@ -3,5 +3,3 @@ FROM node:12-alpine
 WORKDIR /app
 
 RUN yarn install
-
-EXPOSE 3000
--- src/config/database.js
+++ src/config/database.js
@@ -1,12 +1,14 @@
+const environment = process.env.NODE_ENV || 'development';
+
 module.exports = {
   host: process.env.DB_HOST || 'postgres',
   username: process.env.DB_USER || 'postgres',
   password: process.env.DB_PASSWORD || 'postgres',
-  database: process.env.DB_NAME || 'app_development',
+  database: process.env.DB_NAME || `app_${environment}`,
   dialect: process.env.DB_DIALECT || "postgres",
   define: {
     timestamps: true,
     underscored: true,
     underscoredAll: true
-  }
+  },
 };
