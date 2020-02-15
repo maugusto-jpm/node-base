3e9d81ab26d8ba4ce483064031864b36ce293bf0
Marcelo Nascimento <maugusto.jpm@gmail.com>
2020-02-14 21:43:16 -0300

    Added env to db

--- src/config/database.js
+++ src/config/database.js
@@ -1,9 +1,9 @@
 module.exports = {
-  dialect: 'postgres',
-  host: 'postgres',
-  username: 'postgres',
-  password: 'postgres',
-  database: 'app_development',
+  host: process.env.DB_HOST || 'postgres',
+  username: process.env.DB_USER || 'postgres',
+  password: process.env.DB_PASSWORD || 'postgres',
+  database: process.env.DB_NAME || 'app_development',
+  dialect: process.env.DB_DIALECT || "postgres",
   define: {
     timestamps: true,
     underscored: true,
